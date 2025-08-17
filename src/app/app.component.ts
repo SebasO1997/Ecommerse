eb368cdf6cfd2b0d92c1223c9e861c31184b09ff
SebasO1997 <sebastianojeda970922@gmail.com>
2025-08-17 11:22:24 -0500

    feat: initial basic app component of store

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './domains/shared/components/header/header.component';
+import { LayoutComponent } from "@shared/components/layout/layout.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, LayoutComponent],
+  // template: '<header><router-outlet/></header>',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
